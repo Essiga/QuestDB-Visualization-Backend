20baee12578c48e3f23395186873f7dadaacbbc9
Adrian <Adrian.Essig@outlook.com>
2023-04-14 16:28:14 +0200

    Added functionality for both energy and movement data.

--- src/main/java/at/fhv/QuestDBVisualizationBackend/view/QuestDbRestController.java
+++ src/main/java/at/fhv/QuestDBVisualizationBackend/view/QuestDbRestController.java
@@ -2,7 +2,6 @@ package at.fhv.QuestDBVisualizationBackend.view;
 
 
 import at.fhv.QuestDBVisualizationBackend.application.dto.TimeFrameDTO;
-import io.questdb.client.Sender;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -22,78 +19,105 @@ import java.util.stream.IntStream;
 
 @CrossOrigin(origins="*")
 @RestController
-@RequestMapping("/rest")
+@RequestMapping("/rest/questdb")
 public class QuestDbRestController {
 
     @Autowired
     private Environment environment;
 
-    private static final String TEST = "/test";
+    private static final String MOVEMENT_GET_BY_TIMEFRAME = "/movement/getByTimeFrame";
+    private static final String ENERGY_GET_BY_TIMEFRAME = "/energy/getByTimeFrame";
 
     private long convertToEpochMicro(Instant time){
         return TimeUnit.SECONDS.toMicros(time.getEpochSecond()) + TimeUnit.NANOSECONDS.toMicros(time.getNano());
     }
 
-    @PostMapping(TEST)
-    public String Test(@RequestBody TimeFrameDTO timeFrame) throws SQLException {
-
-        long epochStartDate = convertToEpochMicro(timeFrame.getStartDate());
-        long epochEndDate = convertToEpochMicro(timeFrame.getEndDate());
+    private long convertToEpochNano(Instant time){
+        return TimeUnit.SECONDS.toNanos(time.getEpochSecond()) + (time.getNano());
+    }
 
 
-        System.out.println("Debug:");
-        System.out.println("-------------------------------");
-        System.out.println("startDate: " + epochStartDate);
-        System.out.println("endDate: " + epochEndDate);
-
-        JSONArray result = new JSONArray();
-
+    private Connection connectToQuestDB() throws SQLException {
         Properties properties = new Properties();
         properties.setProperty("user", environment.getProperty("questdb.username"));
         properties.setProperty("password", environment.getProperty("questdb.password"));
         properties.setProperty("sslmode", "disable");
 
-        final Connection connection = DriverManager.getConnection(
+        return DriverManager.getConnection(
                 "jdbc:postgresql://"+ environment.getProperty("questdb.ipaddress") +"/qdb", properties);
+    }
+
+    @PostMapping(MOVEMENT_GET_BY_TIMEFRAME)
+    public String getMovementDataByTimeFrame(@RequestBody TimeFrameDTO timeFrame) throws SQLException {
+
+        long epochStartDate = convertToEpochMicro(timeFrame.getStartDate());
+        long epochEndDate = convertToEpochMicro(timeFrame.getEndDate());
+
+        JSONArray result;
+
+        final Connection connection = connectToQuestDB();
+
         try (PreparedStatement preparedStatement = connection.prepareStatement(
                 "SELECT * FROM movement_data WHERE ValuesTS_PLC > ? AND ValuesTS_PLC < ?")) {
-            preparedStatement.setLong(1, epochStartDate);
-            preparedStatement.setLong(2, epochEndDate);
-            try (ResultSet rs = preparedStatement.executeQuery()) {
-                ResultSetMetaData md = rs.getMetaData();
-                int numCols = md.getColumnCount();
-                List<String> colNames = IntStream.range(0, numCols)
-                        .mapToObj(i -> {
-                            try {
-                                return md.getColumnName(i + 1);
-                            } catch (SQLException e) {
-                                e.printStackTrace();
-                                return "?";
-                            }
-                        })
-                        .collect(Collectors.toList());
-
-
-                while (rs.next()) {
-                    JSONObject row = new JSONObject();
-                    colNames.forEach(cn -> {
-                        try {
-                            row.put(cn, rs.getObject(cn));
-                        } catch (JSONException | SQLException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                    result.put(row);
-                }
-
-//                while (rs.next()) {
-//                    System.out.println(rs.getString(1));
-//                }
-            }
+            result = executePreparedStatementForTimeFrame(epochStartDate, epochEndDate, preparedStatement);
         }
         connection.close();
-
-
         return result.toString();
+    }
+
+    @PostMapping(ENERGY_GET_BY_TIMEFRAME)
+    public String getEnergyDataByTimeFrame(@RequestBody TimeFrameDTO timeFrame) throws SQLException {
+
+        long epochStartDate = convertToEpochNano(timeFrame.getStartDate());
+        long epochEndDate = convertToEpochNano(timeFrame.getEndDate());
+
+        JSONArray result;
+
+        final Connection connection = connectToQuestDB();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM energy_data WHERE ValuesTS > ? AND ValuesTS < ?")) {
+            result = executePreparedStatementForTimeFrame(epochStartDate, epochEndDate, preparedStatement);
+        }
+        connection.close();
+        return result.toString();
+    }
+
+    private List<String> getColNames(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int numCols = md.getColumnCount();
+        return IntStream.range(0, numCols)
+                .mapToObj(i -> {
+                    try {
+                        return md.getColumnName(i + 1);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        return "?";
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    private JSONArray executePreparedStatementForTimeFrame(long epochStartDate, long epochEndDate, PreparedStatement preparedStatement) throws SQLException {
+        JSONArray result = new JSONArray();
+
+        preparedStatement.setLong(1, epochStartDate);
+        preparedStatement.setLong(2, epochEndDate);
+        try (ResultSet rs = preparedStatement.executeQuery()) {
+            List<String> colNames = getColNames(rs);
+
+            while (rs.next()) {
+                JSONObject row = new JSONObject();
+                colNames.forEach(cn -> {
+                    try {
+                        row.put(cn, rs.getObject(cn));
+                    } catch (JSONException | SQLException e) {
+                        e.printStackTrace();
+                    }
+                });
+                result.put(row);
+            }
+        }
+        return result;
     }
 }
