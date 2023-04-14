960051b68d79d780ff7ea855111e0c4832416c2d
Adrian <Adrian.Essig@outlook.com>
2023-04-14 18:48:18 +0200

    Connection to MongoDB finally working

--- src/main/java/at/fhv/QuestDBVisualizationBackend/view/MongoDbRestController.java
+++ src/main/java/at/fhv/QuestDBVisualizationBackend/view/MongoDbRestController.java
@@ -1,18 +1,36 @@
 package at.fhv.QuestDBVisualizationBackend.view;
 
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/rest/mongodb")
 public class MongoDbRestController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/test")
     public String test(){
+        String connectionString = environment.getProperty("mongodb.connectionstring");
 
-        return "HelloWorld :)";
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+
+        return "hi";
     }
 }
