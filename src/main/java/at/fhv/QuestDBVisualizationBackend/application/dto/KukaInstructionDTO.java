8bcd4e92d6f45010d396af0582e07d66e5c612f0
Adrian <Adrian.Essig@outlook.com>
2023-04-23 00:58:48 +0200

    getting only start and end data finally works

--- src/main/java/at/fhv/QuestDBVisualizationBackend/application/dto/KukaInstructionDTO.java
+++ src/main/java/at/fhv/QuestDBVisualizationBackend/application/dto/KukaInstructionDTO.java
@@ -1,11 +1,71 @@
 package at.fhv.QuestDBVisualizationBackend.application.dto;
 
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+
 public class KukaInstructionDTO {
-//    private final String name;
-//    private final Variant value;
-//    private final StatusCode status;
-//    private final DateTime sourceTime;
-//    private final UShort sourcePicoseconds;
-//    private final DateTime serverTime;
-//    private final UShort serverPicoseconds;
+    private String name;
+    private boolean value;
+    private LocalDateTime sourceTime;
+    private LocalDateTime serverTime;
+    private int trayPosCap;
+    private int trayPosBearing;
+    private ObjectId mongoDbObjectId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getSourceTime() {
+        return sourceTime;
+    }
+
+    public void setSourceTime(LocalDateTime sourceTime) {
+        this.sourceTime = sourceTime;
+    }
+
+    public LocalDateTime getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(LocalDateTime serverTime) {
+        this.serverTime = serverTime;
+    }
+
+    public int getTrayPosCap() {
+        return trayPosCap;
+    }
+
+    public void setTrayPosCap(int trayPosCap) {
+        this.trayPosCap = trayPosCap;
+    }
+
+    public int getTrayPosBearing() {
+        return trayPosBearing;
+    }
+
+    public void setTrayPosBearing(int trayPosBearing) {
+        this.trayPosBearing = trayPosBearing;
+    }
+
+    public ObjectId getMongoDbObjectId() {
+        return mongoDbObjectId;
+    }
+
+    public void setMongoDbObjectId(ObjectId mongoDbObjectId) {
+        this.mongoDbObjectId = mongoDbObjectId;
+    }
 }
--- src/main/java/at/fhv/QuestDBVisualizationBackend/infrastructure/MongoDbRepositoryImpl.java
+++ src/main/java/at/fhv/QuestDBVisualizationBackend/infrastructure/MongoDbRepositoryImpl.java
@@ -1,5 +1,6 @@
 package at.fhv.QuestDBVisualizationBackend.infrastructure;
 
+import at.fhv.QuestDBVisualizationBackend.application.dto.KukaInstructionDTO;
 import at.fhv.QuestDBVisualizationBackend.domain.repositories.MongoDbRepository;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -13,9 +14,11 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.bson.codecs.configuration.*;
-import org.bson.codecs.pojo.*;
 import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 @Component
 public class MongoDbRepositoryImpl implements MongoDbRepository {
@@ -45,7 +48,7 @@ public class MongoDbRepositoryImpl implements MongoDbRepository {
         ObjectId end = new ObjectId(endTimestamp.getTime(), endTimestamp.getInc());
 
         Bson bsonFilter = Filters.and(
-                Filters.eq("name", "execute"),
+                Filters.eq("name", "fullAssembly"),
                 Filters.gte("_id", start),
                 Filters.lte("_id", end));
         FindIterable<Document> findIt = collection.find(bsonFilter);
@@ -54,10 +57,61 @@ public class MongoDbRepositoryImpl implements MongoDbRepository {
 
         JSONArray result = new JSONArray();
 
+
+        List<KukaInstructionDTO> kukaInstructions = new LinkedList<>();
+
+
         for (Document document : findIt) {
+            KukaInstructionDTO kukaInstruction = new KukaInstructionDTO();
+            kukaInstruction.setName(document.getString("name"));
+            kukaInstruction.setMongoDbObjectId((ObjectId) document.get("_id"));
+            kukaInstruction.setValue(((Document)document.get("value")).getBoolean("value"));
+            if(kukaInstructions.size() == 135){
+                System.out.println("now");
+            }
+            kukaInstructions.add(kukaInstruction);
+
             JSONObject row = new JSONObject(document.toJson());
             result.put(row);
         }
+
+        //after it finishes there should be all start and end timestamps in the list
+        boolean positiveFound = false;
+        List<KukaInstructionDTO> relevantData = new LinkedList<>();
+        int counter = 0;
+        while(kukaInstructions.size() > counter){
+            if(positiveFound == false && kukaInstructions.get(counter).getValue() == true){
+                positiveFound = true;
+                relevantData.add(kukaInstructions.get(counter));
+            } else if (positiveFound == true && kukaInstructions.get(counter).getValue() == false) {
+                positiveFound = false;
+                relevantData.add(kukaInstructions.get(counter));
+            }
+            counter++;
+
+        }
+
+//        for (int i = 0; i < kukaInstructions.size(); i++) {
+////            if(kukaInstructions.get(i).getValue().equals("0")){
+////                kukaInstructions.remove(kukaInstructions.get(i));
+////            }
+//            while(positiveFound == false && kukaInstructions.size() > i) {
+//                if(kukaInstructions.get(i).getValue() == true){
+//                    relevantData.add(kukaInstructions.get(i));
+//                    positiveFound = true;
+//                } else {
+//                    i++;
+//                }
+//            }
+//            while (positiveFound == true && kukaInstructions.size() > i){
+//                if(kukaInstructions.get(i).getValue() == false){
+//                    relevantData.add(kukaInstructions.get(i));
+//                    positiveFound = false;
+//                } else {
+//                    i++;
+//                }
+//            }
+//        }
 
         return result;
     }
